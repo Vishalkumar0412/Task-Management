a552cb8638b3415ddc9d52c7feabcbfdb41a5b61
vishal kumar <vishakumar041200@gmail.com>
2025-07-18 10:18:16 +0530

    Task management

--- backend/controller/controller.js
+++ backend/controller/controller.js
@@ -1,3 +1,13 @@
+function readTasks() {
+  return JSON.parse(fs.readFileSync(DATA_FILE, 'utf-8') || '[]');
+}
+
+
+function writeTasks(tasks) {
+  fs.writeFileSync(DATA_FILE, JSON.stringify(tasks, null, 2));
+}
+
+
 export const deleteTask=(req, res) => {
   const { id } = req.params;
   let tasks = readTasks();
@@ -19,4 +29,14 @@ export const updateTask =(req, res) => {
 export const getTask= (req, res) => {
   const tasks = readTasks();
   res.json(tasks);
+}
+export const addTask=(req, res) => {
+  const { title, dueDate, status } = req.body;
+  if (!title || !dueDate) return res.status(400).json({ message: 'Title and Due Date required' });
+
+  const tasks = readTasks();
+  const newTask = { id: Date.now(), title, dueDate, status: status || 'pending' };
+  tasks.push(newTask);
+  writeTasks(tasks);
+  res.status(201).json(newTask);
 }
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,35 +2,19 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs')
 const path = require('path');
+const { getTask, updateTask, deleteTask, addTask } = require('../controller/controller.js');
 const DATA_FILE = path.join(__dirname, '../tasks.json');
 
-function readTasks() {
-  return JSON.parse(fs.readFileSync(DATA_FILE, 'utf-8') || '[]');
-}
+
+router.post('/tasks', addTask);
 
 
-function writeTasks(tasks) {
-  fs.writeFileSync(DATA_FILE, JSON.stringify(tasks, null, 2));
-}
-
-router.post('/tasks', (req, res) => {
-  const { title, dueDate, status } = req.body;
-  if (!title || !dueDate) return res.status(400).json({ message: 'Title and Due Date required' });
-
-  const tasks = readTasks();
-  const newTask = { id: Date.now(), title, dueDate, status: status || 'pending' };
-  tasks.push(newTask);
-  writeTasks(tasks);
-  res.status(201).json(newTask);
-});
+router.get('/tasks',getTask);
 
 
-router.get('/tasks',);
+router.put('/tasks/:id',updateTask );
 
 
-router.put('/tasks/:id', );
-
-
-router.delete('/tasks/:id', );
+router.delete('/tasks/:id',deleteTask );
 
 module.exports = router;
